53d533215e84165a7d8b2b6a4971175f7c8924ef
Jimmy Leung <hkleungai@connect.ust.hk>
2022-10-31 18:51:29 +0800

    feat: swap recent pick to menu top

--- src/Widget.ts
+++ src/Widget.ts
@@ -28,15 +28,20 @@ class Widget {
     return this.#tray_icon;
   }
 
-  static #main_menu: Menu;
+  // NO caching here, since template will be re-built upon change in `main_menu_template`
   static get main_menu() {
-    if (this.#main_menu) {
-      return this.#main_menu;
+    return Menu.buildFromTemplate(this.main_menu_template);
+  }
+
+  static #main_menu_template: MenuItemConstructorOptions[];
+  static get main_menu_template() {
+    if (this.#main_menu_template) {
+      return this.#main_menu_template;
     }
 
     const sticker_folders = fs.readdirSync(FileSystem.sticker);
 
-    const contextMenuTemplate = sticker_folders.map<MenuItemConstructorOptions>(folder => {
+    this.#main_menu_template = sticker_folders.map<MenuItemConstructorOptions>(folder => {
       const absolute_folder = path.resolve(FileSystem.sticker, folder);
       const png_files = fs.readdirSync(absolute_folder).filter(file => /\.png/.test(file));
 
@@ -44,22 +49,44 @@ class Widget {
       const icon = Operation.create_image(last_png_file, 100);
 
       const submenu = png_files.map(png_file => ({
+        id: png_file,
         label: ' ',
         icon: Operation.create_image(path.resolve(absolute_folder, png_file), 100),
         click: () => {
           this.success_copy_banner.show();
           const gif_file = path.resolve(absolute_folder, png_file.replace(/\.png/, '.gif'));
           Operation.copy_gif(gif_file);
+          this.#swap_recent_pick_to_menu_top(folder, png_file);
           setTimeout(() => this.success_copy_banner.close(), 2000);
         },
       }));
 
-      return { label: ' ', icon, submenu }
+      return {
+        id: folder,
+        label: ' ',
+        icon,
+        submenu
+      }
     });
 
-    this.#main_menu = Menu.buildFromTemplate(contextMenuTemplate);
+    return this.#main_menu_template;
+  };
 
-    return this.#main_menu;
+  static #swap_recent_pick_to_menu_top(folder_id: string, png_file_id: string) {
+    const folder_index = this.main_menu_template.findIndex(({ id }) => id === folder_id);
+    const folder = this.main_menu_template[folder_index];
+    const submenu = folder.submenu as MenuItemConstructorOptions[];
+
+    const png_file_index = submenu.findIndex(({ id }) => id === png_file_id);
+    const png_file = submenu[png_file_index];
+
+    submenu.splice(png_file_index, 1);
+    submenu.unshift(png_file);
+
+    this.main_menu_template.splice(folder_index, 1);
+    this.main_menu_template.unshift(folder);
+
+    this.tray_icon.setContextMenu(this.main_menu);
   }
 }
 
